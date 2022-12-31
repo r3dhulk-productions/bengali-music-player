128499a85aa9a9067b9d43ddbcc0de237c0cc04e
r3dhulk-productions <118876340+r3dhulk-productions@users.noreply.github.com>
2022-12-31 22:13:32 +0530

    bengali songs listed music app

---
+++ js/music-list.js
@@ -0,0 +1,100 @@
+let allMusic = [
+  {
+    name: "Nesha Bojha",
+    artist: "Popeye",
+    img: "aj-ami-sob-harano",
+    src: "aj-ami-sob-harano"
+  },
+  {
+    name: "Utsorgo",
+    artist: "Tansif Zaman",
+    img: "utsorgo",
+    src: "utsorgo"
+  },
+  {
+    name: "Karone Okarone",
+    artist: "Minar Rehman",
+    img: "karone-okarone",
+    src: "karone-okarone"
+  },
+  {
+    name: "Obosthan",
+    artist: "Rakib Hasan",
+    img: "obosthan",
+    src: "obosthan"
+  },
+  {
+    name: "Ogni Kabbo",
+    artist: "Niloy Jubayer",
+    img: "ogni kabbo",
+    src: "ogni kabbo"
+  },
+  {
+    name: "Sondhe Namar Aage",
+    artist: "Ishan Mitra",
+    img: "tumi-jao",
+    src: "tumi-jao"
+  },
+  {
+    name: "Dhoro Jodi Hothat Sondhey",
+    artist: "Spandan Bhattacharya",
+    img: "dhoro-jdi",
+    src: "dhoro-jdi"
+  },
+  {
+    name: "Ayna",
+    artist: "Tanveer Evan",
+    img: "ayna",
+    src: "ayna"
+  },
+  {
+    name: "Ovijog",
+    artist: "Tanveer Evan",
+    img: "ovijog",
+    src: "ovijog"
+  },
+  {
+    name: "Oviman",
+    artist: "Tanveer Evan",
+    img: "oviman",
+    src: "oviman"
+  },
+  {
+    name: "Alote Chol",
+    artist: "Debayan Bhattacharya",
+    img: "alote-chol",
+    src: "alote-chol"
+  },
+  {
+    name: "Amake Nao",
+    artist: "Debayan Bhattacharya",
+    img: "amake nao",
+    src: "amake nao"
+  },
+  {
+    name: "Srotoshrini",
+    artist: "Shakib-Ul-Islam",
+    img: "srotoshrini",
+    src: "srotoshrini"
+  },
+  {
+    name: "Mombati",
+    artist: "Ankita Ishrat",
+    img: "mombati",
+    src: "mombati"
+  },
+  {
+    name: "Ghum",
+    artist: "Moontasir Rakib",
+    img: "ghum",
+    src: "ghum"
+  },
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg format and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mpeg format and it's inside the songs folder of this project folder"
+  // }
+];
